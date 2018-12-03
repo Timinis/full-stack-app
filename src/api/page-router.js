fb9be35fd17b75230ba333c4614aff49b5ff7d67
Timinis <litimc3@gmail.com>
2018-12-02 21:10:47 -0800

    added fix again

--- src/api/page-router.js
+++ src/api/page-router.js
@@ -8,9 +8,17 @@
 import express from 'express';
 const pageRouter = express.Router();
 
+let sendJSON = (response, data) => {
+  response.statusCode = 200;
+  response.statusMessage = 'OK';
+  response.setHeader('Content-Type', 'application/json');
+  response.write(JSON.stringify(data));
+  response.end();
+};
+
 pageRouter.get('/', async (req, res, next) => {
   try {
-    res.send('Server Home');
+    sendJSON(res, 'Server Home');
   } catch (err) {
     next();
   }
